6cbbd5512a10ecdbd289879701f9969aea30199e
xixi <458927520@qq.com>
2017-04-05 17:22:02 +0800

    some

--- SpringImpl/src/dao/Dao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-/**
- * Created by xixi on 20/01/2017.
- */
-public interface Dao {
-    public void daoMethod();
-}
--- SpringImpl/src/service/Service.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-/**
- * Created by xixi on 20/01/2017.
- */
-public interface Service {
-    public void serviceMethod();
-}
